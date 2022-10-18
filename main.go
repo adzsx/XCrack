d428de1bcf68df23119c3ee6902001f932c66bf7
adzsx <adzsx@proton.me>
2022-10-18 20:06:15 +0200

    Wordlist support done

--- main.go
+++ main.go
@@ -8,22 +8,37 @@ import (
 	"os"
 )
 
+var (
+	type_    string
+	password string
+	islist   bool
+	path     string
+)
+
 func main() {
 	args := os.Args
-	help := "\nBrute forcer cracks hashed passwords:\n------------------------------------------------------------------------------------------\n-h: 			Shows this message (ignores other arguments)\n-t HASH:		specify the type of HASH: (md5 (Default), sha1, sha256, sha512)\n-n: 			numbers\n-l: 			lowercase letters\n-L: 			uppercase letters\n-s: 			special Characters\n-m LENGTH: 		min LENGTH of password\n-M LENGTH: 		max LENGTH of password\n-w PATH:		uses a wordlist in PATH (ignores other arguments)\n------------------------------------------------------------------------------------------\n"
+	help := "\nBrute forcer cracks hashed passwords:\n------------------------------------------------------------------------------------------\n-h: 			Shows this message (ignores other arguments)\n-p HASH:		(required) Sets the HASH\n-t HASH-TYPE:		(required) specify the HASH-TYPE: (md5, sha1, sha256, sha512)\n-n: 			numbers\n-l: 			lowercase letters\n-L: 			uppercase letters\n-s: 			special Characters\n-m LENGTH: 		min LENGTH of password\n-M LENGTH: 		max LENGTH of password\n-w PATH:		uses a wordlist in PATH (ignores other arguments)\n------------------------------------------------------------------------------------------\n"
 	args[0] = "Hash-Cracker"
 	for n, element := range args {
 		if element == "-h" {
 			fmt.Println(help)
 			break
+		} else if element == "-t" {
+			type_ = args[n+1]
+		} else if element == "-p" {
+			password = args[n+1]
 		} else if element == "-w" {
-			list := args[n+1]
-			wordlist("test", list)
+			path = args[n+1]
+			islist = true
 		}
+	}
+	if islist {
+		wordlist(password, type_, path)
 	}
 }
 
-func wordlist(preHash string, path string) {
+func wordlist(password string, type_ string, path string) {
+	fmt.Println(password, type_, path)
 	file, err := os.Open(path)
 	if err != nil {
 		fmt.Printf("Path \"%v\" found. Plase enter a valid path!\n", path)
@@ -35,7 +50,7 @@ func wordlist(preHash string, path string) {
 
 		for fileScanner.Scan() {
 			data := fileScanner.Text()
-			if hash(data) == preHash {
+			if hash(type_, data) == password {
 				fmt.Printf("Password: %v \n", data)
 				os.Exit(0)
 			}
@@ -45,7 +60,12 @@ func wordlist(preHash string, path string) {
 	file.Close()
 }
 
-func hash(text string) string {
-	hash := md5.Sum([]byte(text))
-	return hex.EncodeToString(hash[:])
+func hash(form string, text string) string {
+	if form == "md5" {
+		hash := md5.Sum([]byte(text))
+		return hex.EncodeToString(hash[:])
+	}
+	return ""
 }
+
+//530ea1472e71035353d32d341ecf6343
