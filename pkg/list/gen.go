2309c4849f0db76a63d6627d4c09c6be900a3343
bokunodev <bokunocode@gmail.com>
2022-11-25 00:40:46 +0700

    fixed some bugs

--- pkg/list/gen.go
+++ pkg/list/gen.go
@@ -8,30 +8,29 @@ import (
 	"strings"
 	"time"
 
-	"github.com/adzsx/xcrack/format"
+	"github.com/adzsx/xcrack/pkg/check"
+	"github.com/adzsx/xcrack/pkg/format"
 )
 
-var (
-	chars []string
-)
+var chars []string
 
 func WgenSetup(args [6]string, path string) {
 	fmt.Println("Starting wordlist generation mode.")
 
-	//some variables for generating the wordlist
+	// some variables for generating the wordlist
 	file, _ := os.Create(path)
 	min, err := strconv.Atoi(args[4])
 	max, err2 := strconv.Atoi(args[5])
 
-	//check errors in string conversion
+	// check errors in string conversion
 	check.Err(err)
 	check.Err(err2)
 
-	//Create list with characters included in the password
+	// Create list with characters included in the password
 
 	chars = format.CharList(args)
 
-	//length of passwords to be generated
+	// length of passwords to be generated
 	jobs := make(chan int, max-min)
 	response := make(chan bool, max-min)
 
@@ -50,7 +49,7 @@ func WgenSetup(args [6]string, path string) {
 		finished = append(finished, i)
 		if len(finished) > max-min {
 			fmt.Println("Done")
-			fmt.Printf("\n[%v]\n", time.Since(main.Now))
+			fmt.Printf("\n[%v]\n", time.Since(time.Now()))
 			os.Exit(0)
 		}
 	}
@@ -58,10 +57,10 @@ func WgenSetup(args [6]string, path string) {
 
 // Wordlist generation mode
 func gen(chars []string, jobs <-chan int, response chan<- bool, file *os.File) {
-	//main.Chars = characters for password
-	//hashed = hashed password to crack
-	//length = length of characters in main.Chars
-	//jobs = jobs for lengths for multiple gorutines
+	// main.Chars = characters for password
+	// hashed = hashed password to crack
+	// length = length of characters in main.Chars
+	// jobs = jobs for lengths for multiple gorutines
 
 	for currentLength := range jobs {
 		counter := make([]int, currentLength)
@@ -73,7 +72,6 @@ func gen(chars []string, jobs <-chan int, response chan<- bool, file *os.File) {
 			counter[0] += 1
 
 			for index, value := range counter {
-
 				if value > len(chars)-1 {
 					counter[index] = 0
 
@@ -97,7 +95,6 @@ func gen(chars []string, jobs <-chan int, response chan<- bool, file *os.File) {
 
 	}
 	response <- true
-
 }
 
 func contains(s [6]string, element string) bool {
@@ -109,7 +106,7 @@ func contains(s [6]string, element string) bool {
 	return false
 }
 
-func check(err error) {
+func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
